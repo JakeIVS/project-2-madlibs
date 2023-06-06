6862814718e4e59c98f5eb0a1fad924819d44144
Jacob Ives <jacob.w.ives@gmail.com>
2023-06-06 13:26:12 -0600

    add save functionality

--- src/components/FinishedPrompt.jsx
+++ src/components/FinishedPrompt.jsx
@@ -1,6 +1,8 @@
-import { Button, Container, Divider, Segment } from "semantic-ui-react"
+import { useState } from "react"
+import { Button, Container, Divider, Grid, Segment, Icon } from "semantic-ui-react"
 
 function FinishedPrompt({ madLib, filledBlanks, onRestart }) { 
+    const [saved, setSaved] = useState(false)
     let story
     if (!!madLib.template && filledBlanks !== []) {
         let template = madLib.template
@@ -12,14 +14,53 @@ function FinishedPrompt({ madLib, filledBlanks, onRestart }) {
         combinedArray.push(template[template.length-1])
         story = combinedArray?.join('')
     }
+    function handleSave() {
+        setSaved(true)
+        fetch('http://localhost:3000/saved', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "name": madLib.name,
+                "story": story
+            })
+        })
+    }
+    const unSavedBtn = (
+        <Button primary animated='fade'>
+            <Button.Content visible >Save Story</Button.Content>
+            <Button.Content hidden onClick={handleSave}>
+                <Icon name='download'/>
+            </Button.Content>
+        </Button>
+    )
+    const savedBtn = (
+        <Button color="green" icon labelPosition="right">
+            Saved
+            <Icon name="check circle" />
+        </Button>
+    )
     return(
         <Segment piled >
+            <Grid columns={2}>
+                <Grid.Column textAlign="left">
+                    <Button secondary animated='fade' onClick={onRestart}>
+                        <Button.Content visible>Restart</Button.Content>
+                        <Button.Content hidden>
+                            <Icon name="undo" />
+                        </Button.Content>
+                    </Button>
+                </Grid.Column>
+                <Grid.Column textAlign="right">
+                    {saved ? savedBtn : unSavedBtn}
+                </Grid.Column>
+            </Grid>
             <Container text>
                 <h2>{!!madLib.name ? madLib.name : 'Loading...'}</h2>
                 <p>{!!madLib.template && filledBlanks !==[] ? story : 'Loading...'}</p>
             </Container>
             <Divider/>
-            <Button primary fluid circular onClick={onRestart}>Start Over</Button>
         </Segment>
     )
 }
